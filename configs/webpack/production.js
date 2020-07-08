10fd55939aeaa6d3c34c4018711ba2572d72217a
Frank Parejo <francisco.parejo.lopez@gmail.com>
2020-07-09 00:37:42 +0200

    fix production build.

--- configs/webpack/production.js
+++ configs/webpack/production.js
@@ -1,4 +1,7 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = {
   devtool: 'source-map',
-  plugins: [new webpack.DefinePlugin({})],
+  plugins: [new webpack.DefinePlugin({}), new BundleAnalyzerPlugin()],
 };
